19c8fe39d2a03121b60a7261b7255d04c28341ba
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-04 17:11:25 -0500

    feat adicionamos uam nova biblioteca e colocamos um alert na nossa aplicacao

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import { useAlert } from "react-alert";
+import axios from "axios";
 import CustomInput from "./Custominput";
 import "./AddTask.scss";
 import CustomButton from "./CustomButton";
 const AddTask = () => {
     const [task, setTask] = useState("");
+    const alert = useAlert();
     const onChange = (e) => {
         setTask(e.target.value);
+    };
+
+    const handleTaskAddition = async () => {
+        try {
+            if (task.length == 0) {
+                return alert.error(
+                    "A tarefa precisa de uma descricao para ser adicionada!"
+                );
+            }
+        } catch (error) {}
     };
 
     return (
@@ -16,7 +29,7 @@ const AddTask = () => {
                 value={task}
                 onChange={onChange}
             />
-            <CustomButton>
+            <CustomButton onClick={handleTaskAddition}>
                 <FaPlus size={14} color="#ffffff" />
             </CustomButton>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+// optional configuration
+const options = {
+    // you can also just use 'bottom center'
+    position: positions.BOTTOM_CENTER,
+    timeout: 3000,
+    offset: "30px",
+    // you can also just use 'scale'
+    transition: transitions.SCALE,
+};
 root.render(
     <React.StrictMode>
-        <App />
+        <AlertProvider template={AlertTemplate} {...options}>
+            <App />
+        </AlertProvider>
     </React.StrictMode>
 );
 
