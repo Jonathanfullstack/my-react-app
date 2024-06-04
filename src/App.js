5988c8f66f117394d688d3af02329dee5f7dafac
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-04 09:24:01 -0500

    feat fizemos a conexao do nosso front com o banco de dados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import TaskItem from "./components/TaskItem";
+
+import axios from "axios";
+const App = () => {
+    const [tasks, setTask] = useState([
+        {
+            id: "1",
+            description: "Estudar programacao",
+            isCompleted: false,
+        },
+        {
+            id: "2",
+            description: "Ler",
+            isCompleted: true,
+        },
+    ]);
+
+    useEffect(() => {
+        fetchTask();
+    }, []);
+
+    const fetchTask = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:8000/tasks");
+            setTask(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+            ))}
+        </>
+    );
+};
 
 export default App;
