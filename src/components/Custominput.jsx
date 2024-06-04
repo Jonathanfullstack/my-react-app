d636e674a946d9c3e15f632abdfd68e0f4af692f
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-04 14:22:06 -0500

    feat adicionamos o componente custominput

---
+++ src/components/Custominput.jsx
@@ -0,0 +1,16 @@
+const CustomInput = ({ label, value }) => {
+    return (
+        <div className="custom-input-container">
+            <input type="text" className="custom-input" value={value} />
+            {label ? (
+                <label
+                    className={`${
+                        value.length > 0 ? "shrink" : ""
+                    } custom-input-label`}
+                ></label>
+            ) : null}
+        </div>
+    );
+};
+
+export default Costuminput;
