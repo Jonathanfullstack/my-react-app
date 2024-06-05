6c0603224242ecd63071181a5c954267adcceec5
Jonathan Vinicius <jonathantotini@gmail.com>
2024-06-05 01:20:58 -0500

    perf adicionamos task , e usamos o usememo !

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 
 import "./Tasks.scss";
@@ -6,54 +6,56 @@ import TaskItem from "./TaskItem";
 import AddTask from "./AddTask";
 import { useAlert } from "react-alert";
 function Tasks() {
-    const [tasks, setTask] = useState([]);
-    const alert = useAlert();
-    useEffect(() => {
-        fetchTask();
-    }, []);
+  const [tasks, setTask] = useState([]);
+  const alert = useAlert();
 
-    const fetchTask = async () => {
-        try {
-            const { data } = await axios.get("http://localhost:8000/tasks");
-            setTask(data);
-        } catch (_error) {
-            alert.error("Nao foi possivel recuperar as tarefas!");
-        }
-    };
-    return (
-        <div className="tasks-container">
-            <h2>Minhas Tarefas</h2>
-            <div className="last-tasks">
-                <h3>Ultimas tarefas</h3>
-                <AddTask fetchTasks={fetchTask} />
-                <div className="task-list">
-                    {tasks
-                        .filter((task) => task.isCompleted === false)
-                        .map((lastTask) => (
-                            <TaskItem
-                                key={lastTask._id}
-                                task={lastTask}
-                                fetchTask={fetchTask}
-                            />
-                        ))}
-                </div>
-            </div>
-            <div className="completed-tasks">
-                <h3>Tarefas Concluidas</h3>
-                <div className="task-list">
-                    {tasks
-                        .filter((task) => task.isCompleted)
-                        .map((completedTask) => (
-                            <TaskItem
-                                key={completedTask._id}
-                                task={completedTask}
-                                fetchTask={fetchTask}
-                            />
-                        ))}
-                </div>
-            </div>
+  const fetchTask = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:8000/tasks");
+      setTask(data);
+    } catch (_error) {
+      alert.error("Nao foi possivel recuperar as tarefas!");
+    }
+  };
+  const lastTask = useMemo(() => {
+    return tasks.filter((task) => task.isCompleted === false);
+  }, [tasks]);
+  const completedTask = useMemo(() => {
+    return tasks.filter((task) => task.isCompleted === true);
+  }, [tasks]);
+  useEffect(() => {
+    fetchTask();
+  }, []);
+  return (
+    <div className="tasks-container">
+      <h2>Minhas Tarefas</h2>
+      <div className="last-tasks">
+        <h3>Ultimas tarefas</h3>
+        <AddTask fetchTasks={fetchTask} />
+        <div className="task-list">
+          {lastTask.map((lastTask) => (
+            <TaskItem
+              key={lastTask._id}
+              task={lastTask}
+              fetchTask={fetchTask}
+            />
+          ))}
         </div>
-    );
+      </div>
+      <div className="completed-tasks">
+        <h3>Tarefas Concluidas</h3>
+        <div className="task-list">
+          {completedTask.map((completedTask) => (
+            <TaskItem
+              key={completedTask._id}
+              task={completedTask}
+              fetchTask={fetchTask}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Tasks;
